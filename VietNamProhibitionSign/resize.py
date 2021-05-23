dab85cd717ddce38ac925b7da913691c1b96919c
thuongle2210 <thuong.le2210@hcmut.edu.vn>
2021-05-23 23:36:01 +0700

    third commit

--- VietNamProhibitionSign/resize.py
+++ VietNamProhibitionSign/resize.py
@@ -1,6 +1,6 @@
 from cv2 import cv2
 for i in range(8):
     for j in range(590):
-        image = cv2.imread("%d/frame%d.jpg"%(i, j))
-        image = cv2.resize(image, (image.shape[1]//(j % 10 + 1), image.shape[0]//(j % 10 + 1)))
+        image = cv2.imread("/home/thuongle2210/Documents/OS/mở rộng/VietNamProhibitionSign (copy)/VietNamProhibitionSign/%d/frame%d.jpg"%(i, j))
+        image = cv2.resize(image, (image.shape[1]//(j % 15 + 1), image.shape[0]//(j % 15 + 1)))
         cv2.imwrite("%d/frame%d.jpg"%(i, j), image)
